aa62a6eeea547158471b00ee9f27f98f346eebbc
zouzarf <zakaria.ouzarf@gmail.com>
2017-12-11 16:32:07 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,83 +12,62 @@ import org.json.simple.parser.ParseException;
 import org.json.simple.parser.JSONParser;
 class Main {
 
-  public static void main(String[] args) throws Exception{
-
-    String USER_AGENT = "Mozilla/5.0";
-    String url = "https://www.metaweather.com/api/location/search/?query=bordeaux";
-
-    URL obj = new URL(url);
-    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-    // optional default is GET
-    con.setRequestMethod("GET");
-
-    //add request header
-    con.setRequestProperty("User-Agent", USER_AGENT);
-
-    int responseCode = con.getResponseCode();
-    //System.out.println("\nSending 'GET' request to URL : " + url);
-    //System.out.println("Response Code : " + responseCode);
-
-    BufferedReader in = new BufferedReader(
-            new InputStreamReader(con.getInputStream()));
-    String inputLine;
-    StringBuffer response = new StringBuffer();
-
-    while ((inputLine = in.readLine()) != null) {
-      response.append(inputLine);
-    }
-    in.close();
-    
-    //System.out.println(response.toString());
-    JSONParser parser = new JSONParser();
+	public static void main(String[] args) throws Exception{
+		int h_y=0;
+		int w_y=0;
+		int nb_days = 1;
+		String city="";
+		for(int i=0; i < args.length; i++) {
+			if(i==0){
+				city=args[0];
+			}
+			else {
+				if(args[i].equals("-j")){
+					i++;
+					nb_days= Integer.parseInt(args[i]);
+				}
+				else if(args[i].equals("-w")){
+					w_y=1;
+					
+				}
+				else if(args[i].equals("-h")){
+					h_y=1;
+					
+				}
+			}
+		  }
 		
-    try{
-       Object z = parser.parse(response.toString());
-       JSONArray array = (JSONArray)z;
-			
-       /*System.out.println("The 2nd element of array");
-       System.out.println(array.get(0));
-       System.out.println();*/
-
-       JSONObject obj2 = (JSONObject)array.get(0);
-       int nb_days = 2;
-       int h_y=1;
-       int w_y=1;
-       System.out.print("+-------------+");
-       for (int i =0;i<nb_days;i++){
-    	   System.out.print("--------+");
-       }
-       System.out.println("");
-       System.out.print("|             |");
-       for (int i =0;i<nb_days;i++){
-    	   System.out.print("  J+"+i+"   |");
-       }
-       System.out.println("");
-    
-       
-       /*System.out.println("Field \"title\"");
-       System.out.println(obj2.get("title"));*/
-       
-       
-       
-       
-       MetaWeather meta = new MetaWeather(nb_days);
-       meta.update(args[0]);
-       meta.description("| MetaWeather |", h_y, w_y);
-       for (int i =0; i<meta.t.length; i++) {
-    	   meta.update(args[0]);
-    	   System.out.println("J+0 temp " + (int)meta.get_t(i) + " humidity "+ meta.get_h(i) + " wind " + (int)meta.get_w(i));   
-       }
+		first_line(nb_days);
 
 
+		MetaWeather meta = new MetaWeather(nb_days);
+		meta.update(city);
+		meta.description("| MetaWeather |", h_y, w_y);
 
-    }catch(ParseException pe){
-		
-       System.out.println("position: " + pe.getPosition());
-       System.out.println(pe);
-    }
+		Prev prev = new Prev(nb_days);
+		prev.update(city);
+		prev.description("|   P-Meteo   |", h_y, w_y);
+		last_line(nb_days);
 
-  }
+	}
+	public static void first_line(int nb_days){
+		System.out.print("+-------------+");
+		for (int i =0;i<nb_days;i++){
+			System.out.print("--------+");
+		}
+		System.out.println("");
+		System.out.print("|             |");
+		for (int i =0;i<nb_days;i++){
+			System.out.print("  J+"+i+"   |");
+		}
+		System.out.println("");
+	}
+	public static void last_line(int nb_days){
+		System.out.print("+-------------+");
+		for (int i =0;i<nb_days;i++){
+			System.out.print("--------+");
+		}
+		System.out.println("");
+	}
 
 }

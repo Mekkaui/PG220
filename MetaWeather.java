8136f637d8024b2f614713a67209f8b2ed8578a8
zouzarf <zakaria.ouzarf@gmail.com>
2017-12-15 17:17:21 +0100

    Update MetaWeather.java

--- MetaWeather.java
+++ MetaWeather.java
@@ -1,9 +1,3 @@
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-
 public class MetaWeather extends Meteo{
 
 	public MetaWeather(int nb){
@@ -11,34 +5,18 @@ public class MetaWeather extends Meteo{
 	}
 
 	void update(String city) throws Exception{
-		DataQuery query1 = new DataQuery("https://www.metaweather.com/api/location/search/?query="+city);
-		JSONParser line1 = new JSONParser();
-		try{
-			Object line1_obj = line1.parse(query1.getData());
-			JSONArray array = (JSONArray)line1_obj;
-
-			JSONObject location_search = (JSONObject)array.get(0);
-			String woeid = String.valueOf(location_search.get("woeid"));
-
-			DataQuery query2 = new DataQuery("https://www.metaweather.com/api/location/"+woeid);
-			JSONParser line2 = new JSONParser();
-			Object line2_obj = line2.parse(query2.getData());
-			JSONArray line2_parsed = new JSONArray();
-			line2_parsed.add(line2_obj);
-			JSONObject location = (JSONObject)line2_parsed.get(0);
-			Object consolidated_weather = location.get("consolidated_weather");
-			for(int i=0;i<super.nb_days;i++)
+		String url = "https://www.metaweather.com/api/location/search/?query="+city;
+		Object line = parse_url(url);
+		Object location_search = parse_array(line,0);
+		Object woeid = parse_object(location_search,"woeid");
+		Object location = parse_url("https://www.metaweather.com/api/location/"+String.valueOf(woeid));
+		Object consolidated_weather = parse_object(location,"consolidated_weather");
+		for(int i=0;i<super.nb_days;i++)
 			{
-				JSONArray c = (JSONArray)consolidated_weather;
-				JSONObject infos = (JSONObject)c.get(i);
-				this.t[i]=(int)(double)(infos.get("the_temp"));
-				this.h[i]=(int)(long)(infos.get("humidity"));
-				this.w[i]=(int)(double)(infos.get("wind_speed"));
+				Object infos = parse_array(consolidated_weather,i);
+				this.t[i]=(int)(double)(parse_object(infos,"the_temp"));
+				this.h[i]=(int)(long)(parse_object(infos,"humidity"));
+				this.w[i]=(int)(double)(parse_object(infos,"wind_speed"));
 			}
-		}catch(ParseException pe){
-
-			System.out.println("position: " + pe.getPosition());
-			System.out.println(pe);
-		}
 	}
 }

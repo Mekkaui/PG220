f34799b62b698fd222ddd62b2fe0d0d56356c80d
zouzarf <zakaria.ouzarf@gmail.com>
2017-12-15 17:17:50 +0100

    Update Yahoo.java

--- Yahoo.java
+++ Yahoo.java
@@ -1,44 +1,37 @@
-import java.util.ArrayList;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
 public class Yahoo extends Meteo{
 
 	public Yahoo(int nb){
 		super(nb);
 	}
 	void update(String city) throws Exception{
-		try{
-			DataQuery x = new DataQuery("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+city+"%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
-			JSONParser parser = new JSONParser();
-			JSONObject line = (JSONObject)parser.parse(x.getData());
-			JSONObject channel = (JSONObject)((JSONObject)((JSONObject)line.get("query")).get("results")).get("channel");
+			String url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+city+"%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+			Object line = parse_url(url);
+			Object query = parse_object(line,"query");
+			Object results = parse_object(query,"results");
+			Object channel = parse_object(results,"channel");
+			
 			// For wind speed
-			this.w[0] =  Integer.parseInt(( (String)( (JSONObject)channel.get("wind") ).get("speed") ).toString());
+			Object wind = parse_object(channel,"wind");
+			Object speed = parse_object(wind,"speed");
+			
 			// For humidity
-			this.h[0] = Integer.parseInt((  (String) ( (JSONObject) channel.get("atmosphere") ).get("humidity")  ).toString());
-			// Pas d'information
-			for (int i=1; i<nb_days; i++)
-			{
-				this.h[i] = -1;
-				this.w[i] = -1;
-			}
+			Object atmosphere = parse_object(channel,"atmosphere");
+			Object humidity = parse_object(atmosphere,"humidity");
+			
 			//For days
-			JSONArray forecast_array = (JSONArray)( (JSONObject)channel.get("item") ).get("forecast");
+			Object item = parse_object(channel,"item");
+			Object forecast_array = parse_object( item ,"forecast");
+			
 			for(int i=0;i<super.nb_days;i++)
 			{
-				JSONObject infos = (JSONObject)forecast_array.get(i);
-				int high = Integer.parseInt((String)infos.get("high"));
-				int low = Integer.parseInt((String)infos.get("low"));
+				Object infos = parse_array(forecast_array,i);
+				int high = Integer.parseInt((String)parse_object(infos,"high"));
+				int low = Integer.parseInt((String)parse_object(infos,"low"));
 				int celc = (((low +high)/2) - 32) * 5/9;
 				this.t[i]= (celc);
 			}
-		}catch(ParseException pe){
-			System.out.println("position: " + pe.getPosition());
-			System.out.println(pe);
-		}
+			this.w[0] =  Integer.parseInt( (String)( speed.toString()));
+			this.h[0] = Integer.parseInt((  (String) ( humidity  ).toString()));
+		
 	}
 }

f9ece84ba62a4d533f9950c2f802d56dd445e7e8
Mohamed Mekkaoui <mohammed.mekkaoui@enseirb-matmeca.fr>
2017-12-18 12:21:17 +0100

    Add files via upload

---
+++ DataQuery.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+public class DataQuery {
+
+	String data;
+	
+	public DataQuery(String url) throws Exception{
+		String USER_AGENT = "Mozilla/5.0";
+	    
+	    URL obj = new URL(url);
+	    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+	    // optional default is GET
+	    con.setRequestMethod("GET");
+
+	    //add request header
+	    con.setRequestProperty("User-Agent", USER_AGENT);
+
+	    int responseCode = con.getResponseCode();
+	    //System.out.println("\nSending 'GET' request to URL : " + url);
+	    //System.out.println("Response Code : " + responseCode);
+
+	    BufferedReader in = new BufferedReader(
+	            new InputStreamReader(con.getInputStream()));
+	    String inputLine;
+	    StringBuffer response = new StringBuffer();
+
+	    while ((inputLine = in.readLine()) != null) {
+	      response.append(inputLine);
+	    }
+	    in.close();
+	    
+	   this.data=response.toString();
+	    
+	}
+	public String getData(){
+		return this.data;
+	}
+}
--- Main.java
+++ Main.java
@@ -1,86 +1,24 @@
+import API.MetaWeather;
+import API.Prev;
+import API.Yahoo;
+import Affichage.Affichage;
+import Affichage.Arguments;
+
 class Main {
 
 	public static void main(String[] args) throws Exception{
-		int h_y=0;
-		int w_y=0;
-		int f_y=0;
-		int nb_days = 1;
-		String city="";
-		for(int i=0; i < args.length; i++) {
-			if(i==0){
-				city=args[0];
-			}
-			else {
-				if(args[i].equals("-j")){
-					i++;
-					nb_days= Integer.parseInt(args[i]);
-				}
-				else if(args[i].equals("-w")){
-					w_y=1;
-					
-				}
-				else if(args[i].equals("-h")){
-					h_y=1;
-					
-				}
-			}
-		  }
 		
-		MetaWeather meta = new MetaWeather(nb_days);
-		Prev prev = new Prev(nb_days);
-		Yahoo yaho = new Yahoo(nb_days);
-		try {
-			meta.update(city);
-		}
-		catch(Exception e) {
-			System.out.println("Error while getting datas from MetaWeather API.");
-		}
-		
-		try {
-			prev.update(city);
-		}
-		catch(Exception e) {
-				System.out.println("Error while getting datas from Prevision-Meteos API.");
-		}
-		
-		try {
-			yaho.update(city);
-		}
-		catch(Exception e) {
-			System.out.println("Error while getting datas from Yahoo API.");
-		}
-		
-		first_line(nb_days);
+		Arguments argms = new Arguments(args);
+		MetaWeather meta = new MetaWeather(argms.nb_days);
+		Yahoo yaho = new Yahoo(argms.nb_days);
+		Prev prev = new Prev(argms.nb_days);
+		meta.update(argms.city);
+		prev.update(argms.city);
+		yaho.update(argms.city);
 
+		Affichage aff = new Affichage(argms,meta,prev,yaho);
+		aff.cmdread();
+	}
 
-		
-		
-		
-		meta.description("| MetaWeather |", h_y, w_y,f_y);
-		prev.description("|   P-Meteo   |", h_y, w_y,f_y);
-		yaho.description("|   Yahoo     |", h_y, w_y,f_y);
-		
-		last_line(nb_days);
-
-	}
-	public static void first_line(int nb_days){
-		System.out.print("+-------------+");
-		for (int i =0;i<nb_days;i++){
-			System.out.print("--------+");
-		}
-		System.out.println("");
-		System.out.print("|             |");
-		for (int i =0;i<nb_days;i++){
-			System.out.print("  J+"+i+"   |");
-		}
-		System.out.println("");
-	}
-	public static void last_line(int nb_days){
-		System.out.print("+-------------+");
-		for (int i =0;i<nb_days;i++){
-			System.out.print("--------+");
-		}
-		System.out.println("");
-	}
 
 }
